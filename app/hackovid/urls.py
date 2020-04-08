600149ccfd68eaa6ee3fc9f0844a01322aa11c10
Sergio <sergio.paredes@thoughtworks.com>
2020-04-08 23:52:26 +0200

    Add namespace in urls for DRF

--- app/hackovid/urls.py
+++ app/hackovid/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 if bool(settings.DEBUG):
