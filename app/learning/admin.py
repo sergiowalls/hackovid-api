61f2f339e7591c3879afbe2ebd9d33c35e8fc537
Sergio <sergio.paredes@thoughtworks.com>
2020-04-09 02:31:02 +0200

    Add basic learning models

---
+++ app/learning/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from learning.models import LearningUnit
+
+admin.site.register(LearningUnit)
--- app/learning/models.py
+++ app/learning/models.py
@@ -1,9 +1,34 @@
+from django.contrib.auth.models import User
 from django.db import models
 from rest_framework import serializers
 
 
+class LearningUnit(models.Model):
+    title = models.fields.CharField(max_length=60)
+    subject = models.fields.CharField(max_length=60)
+    course = models.fields.CharField(max_length=60)
+
+
+class Resource(models.Model):
+    title = models.fields.CharField(max_length=60)
+
+
+class LinkResource(Resource):
+    link = models.fields.URLField()
+    is_video = models.fields.BooleanField()
+
+
+class Section(models.Model):
+    title = models.fields.CharField(max_length=60)
+    description = models.fields.TextField()
+    resources = models.ManyToManyField(Resource, null=True, blank=True)
+
+
 class Class(models.Model):
-    title = models.fields.CharField(max_length=120, blank=False, null=False)
+    title = models.fields.CharField(max_length=120)
+    learning_unit = models.ForeignKey(LearningUnit, on_delete=models.DO_NOTHING)
+    sections = models.ManyToManyField(Section, null=True, blank=True)
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.fields.DateTimeField(auto_now_add=True)
 
 
