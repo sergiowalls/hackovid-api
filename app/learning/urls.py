e41b7ea8a0097d80e189cecb0689b5531de281bc
Sergio <sergio.paredes@thoughtworks.com>
2020-04-11 01:26:14 +0200

    Fix learning urls

--- app/learning/urls.py
+++ app/learning/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
 from learning import views
-from learning.views import LearningUnitsView, LearningUnitView
 
 urlpatterns = [
     path(r'user', views.UserView.as_view()),
     path(r'class', views.ClassesView.as_view()),
     path(r'class/<int:pk>/', views.ClassView.as_view()),
-    path(r'learning-units', views,LearningUnitsView.as_view()),
-    path(r'learning-units/<int:pk>/', views,LearningUnitView.as_view())
+    path(r'learning-unit', views.LearningUnitsView.as_view()),
+    path(r'learning-unit/<int:pk>/', views.LearningUnitView.as_view())
 ]
