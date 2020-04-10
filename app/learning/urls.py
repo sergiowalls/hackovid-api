483ce9617fba44a98ae2cb09df42ff1eedbea208
Sergio <sergio.paredes@thoughtworks.com>
2020-04-11 01:27:50 +0200

    Make plural uls

--- app/learning/urls.py
+++ app/learning/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from learning import views
 
 urlpatterns = [
-    path(r'user', views.UserView.as_view()),
-    path(r'class', views.ClassesView.as_view()),
-    path(r'class/<int:pk>/', views.ClassView.as_view()),
-    path(r'learning-unit', views.LearningUnitsView.as_view()),
-    path(r'learning-unit/<int:pk>/', views.LearningUnitView.as_view())
+    path(r'users', views.UserView.as_view()),
+    path(r'classes', views.ClassesView.as_view()),
+    path(r'classes/<int:pk>/', views.ClassView.as_view()),
+    path(r'learning-units', views.LearningUnitsView.as_view()),
+    path(r'learning-units/<int:pk>/', views.LearningUnitView.as_view())
 ]
