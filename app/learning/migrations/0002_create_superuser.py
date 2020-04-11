48457b0908601c2acb7fac935a0f1fc03ae56808
Sergio <sergio.paredes@thoughtworks.com>
2020-04-11 04:12:56 +0200

    Squash migrations and remove rollback operation for superuser creation

--- app/learning/migrations/0002_create_superuser.py
+++ app/learning/migrations/0002_create_superuser.py
@@ -11,15 +11,10 @@ def forwards_func(apps, schema_editor):
     user.save()
 
 
-def reverse_func(apps, schema_editor):
-    User = apps.get_model("learning", "User")
-    User.objects.get(username='admin').delete()
-
-
 class Migration(migrations.Migration):
     dependencies = [
         ('learning', '0001_initial'),
     ]
     operations = [
-        migrations.RunPython(forwards_func, reverse_func),
+        migrations.RunPython(forwards_func),
     ]
