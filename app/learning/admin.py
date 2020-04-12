893b518284f84b2a59430ad4300494c5e80f9546
Gigi <giselaruzafamado@gmail.com>
2020-04-12 21:53:05 +0200

    fix class to admin

--- app/learning/admin.py
+++ app/learning/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from learning.models import LearningUnit, User, Class
+from learning.models import LearningUnit, User
 
-admin.site.register(LearningUnit,Class)
+admin.site.register(LearningUnit)
 admin.site.register(User, UserAdmin)
