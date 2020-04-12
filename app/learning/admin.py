817c48f1164e496f9ff280bb1e4d17ef19ac1fdc
Gigi <giselaruzafamado@gmail.com>
2020-04-12 21:46:20 +0200

    add class to admin

--- app/learning/admin.py
+++ app/learning/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from learning.models import LearningUnit, User
+from learning.models import LearningUnit, User, Class
 
-admin.site.register(LearningUnit)
+admin.site.register(LearningUnit,Class)
 admin.site.register(User, UserAdmin)
